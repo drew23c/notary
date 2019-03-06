885d4b40e414c81103a1987ea2afb5ad2c6cb708
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-03-05 20:06:07 -0500

    added more information to the info component

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,13 +1,35 @@
 import React, {Component} from 'react';
 import payment from '../pics/payment.png';
 import './info.css';
+import professional from '../pics/professional.png';
+import car from '../pics/car.jpg';
+import pen from '../pics/writingPen.jpg';
 
 export default class Info extends Component{
     render(){
         return(
             <div className="info">
-                <h2>Covered by Errors and Omissions Insurance  policy for your protection.  We accept major credit and debit card, cash pay, cashier's check</h2>
+                <h3>Covered by Errors and Omissions Insurance  policy for your protection.  We accept major credit and debit card, cash pay, cashier's check</h3>
                 <img className="payment" src={payment} />
+                <h3>Owned and operated by New York State licensed Notary, Vivienne Caldwell</h3>
+                <h3>Cost includes charge for travel time, mileage, parking if needed, and gas</h3>
+                <div className="professional-section">
+                    <h2>Professional and Reliable</h2>
+                    <img className="professional" src={professional} />
+                    <h3>We will arrive at your location prepared and ready to perform any notary service you require. We'll be courteous and arrive on time</h3>
+                </div>
+                <div className="travel-section">
+                    <h2>Travel to your location</h2>
+                    <img className="car" src={car} />
+                    <h3>We will come to the location that works best for you</h3>
+                </div>
+                <div className="certified-section">
+                    <h2>Certified</h2>
+                    <img className="pen" src={pen} />
+                    <h3>Specialized in loan documents for Refinance, Home purchase or sales, Home Equity Line of Credit and Reverse Mortgage</h3>  
+                    <h3>Responsibilities include communicating with Escrow companies, Lenders and Signing Services</h3>
+                </div>
+                <h2>Contact for a Quote today <br/>347-858-6383</h2>
             </div>
         )
     }
