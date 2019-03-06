eadbb911e995a94789bad2d323b9c66e6f6da07f
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-03-06 12:55:27 -0500

    positioned the title and changed the colors of the footer

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,6 +6,7 @@ export default class Contact extends Component{
     render(){
         return(
             <div className="contact">
+                <h2>Click to Call</h2>
                 <a href="tel:3478586383"><img className="phone" src={phone} /></a>
                 <h2>Call me at 347-858-6383</h2>
             </div>
