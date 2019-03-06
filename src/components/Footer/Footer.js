9ee09e5f194d4ccab0cbf2e03b13d8d5d5966d5c
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-03-06 11:57:55 -0500

    worked on the css to add background color, font color

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import './footer.css';
 
 export default class Footer extends Component{
     render(){
         return(
-            <div className="contact">
+            <div className="footer">
                 <p><a href="mailto:viviennecaldwell35@gmail.com">viviennecaldwell35@gmail.com</a></p>
             </div>
         )
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -7,14 +7,14 @@ export default class Services extends Component{
             <div className="services">
             <h2>Certified Mobile Notary Signing Agent travels to you.  Our service is strictly mobile by appointment only</h2>
                 <ul>
-                    <li>Making sure legal documents are authentic</li>
-                    <li>Checking the identity of the person signing the documents</li>
-                    <li>Serving as a witness for the documents being signed</li>
-                    <li>Travel to borrower’s homes or offices to complete loan documents for Real Estate, Refinancing and modifications</li>
-                    <li>Ensure that signer is aware and understand the documents he/she is signing</li>
-                    <li>Ensure signer is of sound mind and is not signing under duress</li>
-                    <li>Return all signed documents to Escrow company immediately to ensure that there is no lapse in time to record documents as needed in various entities involved</li>
-                    <li>Fingerprinting background check for a new job or license</li>
+                    <li>~Making sure legal documents are authentic</li>
+                    <li>~Checking the identity of the person signing the documents</li>
+                    <li>~Serving as a witness for the documents being signed</li>
+                    <li>~Travel to borrower’s homes or offices to complete loan documents for Real Estate, Refinancing and modifications</li>
+                    <li>~Ensure that signer is aware and understand the documents he/she is signing</li>
+                    <li>~Ensure signer is of sound mind and is not signing under duress</li>
+                    <li>~Return all signed documents to Escrow company immediately to ensure that there is no lapse in time to record documents as needed in various entities involved</li>
+                    <li>~Fingerprinting background check for a new job or license</li>
                 </ul>
             </div>
         )
