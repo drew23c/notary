269377c0cb75d80f2474d964cdb48abd55a1ece6
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-03-11 13:55:13 -0400

    added the list of services to the services component

--- src/mobile/Screens/components/Services/Services.js
+++ src/mobile/Screens/components/Services/Services.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 
 export default class Services extends Component{
     render(){
         return(
-            <View>
-                <Text>Services</Text>
+            <View className="services">
+            <Text>Certified Mobile Notary Signing Agent travels to you.  Our service is strictly mobile by appointment only</Text>
+                <FlatList
+                    data={[
+                        {key: '~Making sure legal documents are authentic'},
+                        {key: '~Checking the identity of the person signing the documents'},
+                        {key: '~Serving as a witness for the documents being signed'},
+                        {key: '~Travel to borrower’s homes or offices to complete loan documents for Real Estate, Refinancing and modifications'},
+                        {key: '~Ensure that signer is aware and understand the documents he/she is signing'},
+                        {key: '~Ensure signer is of sound mind and is not signing under duress'},
+                        {key: '~Return all signed documents to Escrow company immediately to ensure that there is no lapse in time to record documents as needed in various entities involved'},
+                        {key: '~Fingerprinting background check for a new job or license'},
+                    ]}
+                    renderItem={({item}) => <Text>{item.key}</Text>}
+                    />
             </View>
         )
     }
