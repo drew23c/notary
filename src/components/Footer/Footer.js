f7b567b7350ff826504c5d7d7b56eb54c6abc63a
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-03-11 02:13:10 -0400

    css changes including the width for the form

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import './footer.css';
+import email from '../pics/email.png';
 
 export default class Footer extends Component{
     render(){
         return(
             <div className="footer">
-                <p><a href="mailto:viviennecaldwell35@gmail.com">viviennecaldwell35@gmail.com</a></p>
+                <ul className="footer-list">
+                    <li className="title">VC Mobile Signing Agent</li>
+                    {/* <li><a href="mailto:viviennecaldwell35@gmail.com">viviennecaldwell35@gmail.com</a></li> */}
+                    <li><a className="email-icon" href="mailto:viviennecaldwell35@gmail.com"><img src={email} /></a></li>
+                </ul>
             </div>
         )
     }
