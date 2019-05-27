a71a7f14b5046705a5768755229e13c4bcfc5e72
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-05-27 14:46:49 -0400

    added new logo and favicon to site

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import writerHeader from '../pics/writer-header.jpg';
+import logo from '../pics/logo.png';
 import profile from '../pics/profile.JPG';
 import './header.css';
 
@@ -7,8 +7,8 @@ export default class Header extends Component{
     render(){
         return(
             <div className="header">
-                <h1 className="header-title">VC Mobile Signing Agent</h1>
-                <img className="writer-header" src={writerHeader} />
+                {/* <h1 className="header-title">VC Mobile Signing Agent</h1> */}
+                <img className="writer-header" src={logo} />
                 <img className="profile" src={profile} />
                 <h1 className="name-title">Vivienne Caldwell, Mobile Signing Agent</h1><br/>
             </div>
