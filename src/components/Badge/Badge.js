5c2f9bf09a98a3797d64d610ef72948489086115
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-05-27 14:57:34 -0400

    added my badge to site

---
+++ src/components/Badge/Badge.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Badge extends Component{
+
+    render(){
+        return(
+            <div className="Badge">
+                <h3>Developed by <a href="https://www.andrewcaldwell.site" target="_blank">Andrew Caldwell</a></h3>
+                <a href="https://www.facebook.com/bluecanvascreative/?ref=bookmarks" target="_blank"><h3>Blue Canvas Creative</h3></a>
+            </div>
+        )
+    }
+}
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './form.css';
+import Badge from '../Badge/Badge';
 
 export default class Form extends Component{
     render(){
@@ -7,6 +8,7 @@ export default class Form extends Component{
             <div className="form">
                 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdnfeKQk0k5cBa4j3eI_wSbI_3F4pC22q5ykhabFevCJ_h5Ww/viewform?embedded=true" width="640" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
                 <a href='https://play.google.com/store/apps/details?id=com.vcmobilesigningagent&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img className="badge" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
+                <Badge/>
             </div>
         )
     }
