c3cd148246a177ba704cfa028e4be1159637558f
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-05-27 21:37:16 -0400

    slight change to my badge

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -5,7 +5,7 @@ export default class Badge extends Component{
     render(){
         return(
             <div className="Badge">
-                <h3>Developed by <a href="https://www.andrewcaldwell.site" target="_blank">Andrew Caldwell</a></h3>
+                <h3>Developed by <a href="https://www.andrewcaldwell.site" target="_blank">Andrew Caldwell</a> of</h3>
                 <a href="https://www.facebook.com/bluecanvascreative/?ref=bookmarks" target="_blank"><h3>Blue Canvas Creative</h3></a>
             </div>
         )
