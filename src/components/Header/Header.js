8015049e906eb9b98e80778b95a106e26b60fcf8
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-05-27 21:42:32 -0400

    added back the title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ export default class Header extends Component{
     render(){
         return(
             <div className="header">
-                {/* <h1 className="header-title">VC Mobile Signing Agent</h1> */}
+                <h1 className="header-title">VC Mobile Signing Agent</h1>
                 <img className="writer-header" src={writerHeader} />
                 <img className="profile" src={profile} />
                 <h1 className="name-title">Vivienne Caldwell, Mobile Signing Agent</h1><br/>
