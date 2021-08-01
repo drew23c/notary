ce4756822c974db443ce70b18780ee569220734f
Andrew Caldwell <andrewcaldwell@pursuit.org>
2021-08-01 00:03:35 -0400

    Updates made to notary website

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import writerHeader from '../pics/writer-header.jpg';
-import profile from '../pics/profile.JPG';
+import profile from '../pics/IMG_5617.JPG';
 import './header.css';
+import fingerprint from '../pics/live-scan-fingerprint-scanner.jpg';
 
 export default class Header extends Component{
     render(){
         return(
             <div className="header">
-                <h1 className="header-title">VC Mobile Signing Agent</h1>
+                <h1 className="header-title">VC Mobile Signing Agent/ Livescan Fingerprinting</h1>
                 <img className="writer-header" src={writerHeader} />
+                <img className="writer-header-right" src={fingerprint} />
                 <img className="profile" src={profile} />
                 <h1 className="name-title">Vivienne Caldwell, Mobile Signing Agent</h1><br/>
             </div>
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -5,12 +5,15 @@ import professional from '../pics/professional.png';
 import car from '../pics/car.jpg';
 import pen from '../pics/writingPen.jpg';
 import separator from '../pics/separator.png';
+import livescan from '../pics/Fingerprint.jpg';
+import cert from '../pics/Trained Signing Agent.png';
+import bbb from '../pics/Picture1.jpg';
 
 export default class Info extends Component{
     render(){
         return(
             <div className="info">
-                <h3>Covered by Errors and Omissions Insurance policy for your protection.  We accept major credit and debit card, cash pay, cashier's check</h3>
+                <h3>Covered by Errors and Omissions Insurance policy for your protection.  I accept cash and cashier's check</h3>
                 <img className="payment" src={payment} />
                 <h3>Owned and operated by New York State licensed Notary, Vivienne Caldwell.</h3>
                 <h3>Cost includes a charge for travel time, mileage, parking if needed, and gas.</h3>
@@ -18,13 +21,13 @@ export default class Info extends Component{
                 <div className="professional-section">
                     <h2>Professional and Reliable</h2>
                     <img className="professional" src={professional} />
-                    <h3>We will arrive at your location prepared and ready to perform any notary service you require. We'll be courteous and arrive on time.</h3>
+                    <h3>I will arrive at your location prepared and ready to perform any notary and fingerprinting service you require. I'll be courteous and arrive on time.</h3>
                 </div>
                 <img className="separator" src={separator} />
                 <div className="travel-section">
                     <h2>Travel to your location</h2>
                     <img className="car" src={car} />
-                    <h3>We will meet you at your home, office, school, hospital, rehab, assisted living or anywhere that is convenient for you.</h3>
+                    <h3>I will meet you at your home, office, school, hospital, rehab, assisted living or anywhere that is convenient for you.</h3>
                 </div>
                 <img className="separator" src={separator} />
                 <div className="certified-section">
@@ -61,6 +64,42 @@ export default class Info extends Component{
                     <h3>Responsibilities include communicating with Escrow companies, Lenders and Signing Services.</h3>
                     <h3>Also serving as a Remote Testimony Witness:</h3>
                     <h3>Telephone Deposition where the court or circumstances set  up a conference call, where a Notary will be present to identify the individual providing the testimony.</h3>
+                    <img className="payment" src={livescan} />
+                    <h3>
+                        I can offer ALL of your print scan and notary signing services wherever you are located. I can
+                        provide services in Libraries, Post Offices, Airports, or any other venue of your request.
+                        The benefits my Mobile Fingerprinting and Notary Signing services include convenience of not
+                        having to leave your home or offices.
+                    </h3>
+                    <img className="payment" src={bbb} />
+                    <h2>Mobile Fingerprinting Specializes In:</h2>
+                    <div className="row">
+                        <div className="column">
+                            <li><b>Contractors</b></li>
+                            <li><b>Heathcare Worker Registry</b></li>
+                            <li><b>Nursing Physician, Medical Industry</b></li>
+                            <li><b>Medical Marijuana</b></li>
+                            <li><b>Adoption</b></li>
+                            <li><b>Security Companies</b></li>
+                            <li><b>Financial Service Companies</b></li>
+                        </div>
+                        <div className="column">
+                            <li><b>Auto Dealers</b></li>
+                            <li><b>Tow Truck Drivers</b></li>
+                            <li><b>School District/Education</b></li>
+                            <li><b>Employment Agencies</b></li>
+                            <li><b>Liquor Authority</b></li>
+                            <li><b>Legal Professionals</b></li>
+                        </div>
+                    </div>
+                    <h2>Additional Services:</h2>
+                    <div className="row">
+                        <div className="column">
+                        <li><b>Passport Pictures</b></li>
+                        <li><b>Background Checks</b></li>
+                        </div>
+                    </div>
+                    <img className="payment" src={cert} />
                 </div>
                 <h2>Contact for a Quote today! <br/>347-858-6393</h2>
             </div>
