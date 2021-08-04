809480b0455a775a7502e85f6d000b249ef80529
Drew <andrewcaldwell@ac.c4q.nyc>
2021-08-03 20:22:05 -0400

    Added BBB review button on Form.js

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -5,12 +5,15 @@ import professional from '../pics/professional.png';
 import car from '../pics/car.jpg';
 import pen from '../pics/writingPen.jpg';
 import separator from '../pics/separator.png';
+import livescan from '../pics/Fingerprint.jpg';
+import cert from '../pics/Trained Signing Agent.png';
+import bbb from '../pics/Picture1.jpg';
 
 export default class Info extends Component{
     render(){
         return(
             <div className="info">
-                <h3>Covered by Errors and Omissions Insurance policy for your protection.  We accept major credit and debit card, cash pay, cashier's check</h3>
+                <h3>Covered by Errors and Omissions Insurance policy for your protection.  I accept cash and cashier's check</h3>
                 <img className="payment" src={payment} />
                 <h3>Owned and operated by New York State licensed Notary, Vivienne Caldwell.</h3>
                 <h3>Cost includes a charge for travel time, mileage, parking if needed, and gas.</h3>
@@ -18,13 +21,13 @@ export default class Info extends Component{
                 <div className="professional-section">
                     <h2>Professional and Reliable</h2>
                     <img className="professional" src={professional} />
-                    <h3>We will arrive at your location prepared and ready to perform any notary service you require. We'll be courteous and arrive on time.</h3>
+                    <h3>I will arrive at your location prepared and ready to perform any notary and fingerprinting service you require. I'll be courteous and arrive on time.</h3>
                 </div>
                 <img className="separator" src={separator} />
                 <div className="travel-section">
                     <h2>Travel to your location</h2>
                     <img className="car" src={car} />
-                    <h3>We will meet you at your home, office, school, hospital, rehab, assisted living or anywhere that is convenient for you.</h3>
+                    <h3>I will meet you at your home, office, school, hospital, rehab, assisted living or anywhere that is convenient for you.</h3>
                 </div>
                 <img className="separator" src={separator} />
                 <div className="certified-section">
@@ -61,7 +64,6 @@ export default class Info extends Component{
                     <h3>Responsibilities include communicating with Escrow companies, Lenders and Signing Services.</h3>
                     <h3>Also serving as a Remote Testimony Witness:</h3>
                     <h3>Telephone Deposition where the court or circumstances set  up a conference call, where a Notary will be present to identify the individual providing the testimony.</h3>
-<<<<<<< HEAD
                     <img className="payment" src={livescan} />
                     <h3>
                         I can offer ALL of your print scan and notary signing services wherever you are located. I can
@@ -69,7 +71,6 @@ export default class Info extends Component{
                         The benefits my Mobile Fingerprinting and Notary Signing services include convenience of not
                         having to leave your home or offices.
                     </h3>
-                    <a href="https://www.bbb.org/new-york-city/customer-reviews/notary-public/vc-mobile-signing-agent-in-saint-albans-ny-87150500/add/" target="_blank" rel="nofollow"><img src="https://seal-newyork.bbb.org/customer-reviews/badge-10-bbb-87150500.png" style="border: 0;" alt="VC Mobile Signing Agent BBB Business Review" /></a>
                     <img className="payment" src={bbb} />
                     <h2>Mobile Fingerprinting Specializes In:</h2>
                     <div className="row">
@@ -99,8 +100,6 @@ export default class Info extends Component{
                         </div>
                     </div>
                     <img className="payment" src={cert} />
-=======
->>>>>>> parent of ce47568 (Updates made to notary website)
                 </div>
                 <h2>Contact for a Quote today! <br/>347-858-6393</h2>
             </div>
