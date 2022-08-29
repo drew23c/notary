fcac2f643d840395467b2ab07d6f5bb4069b02e1
Drew <andrewcaldwell@ac.c4q.nyc>
2022-08-29 17:37:51 -0400

    Added qrcode to services section

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import './services.css';
+import qrcode from '../pics/qrCode.png';
 
 export default class Services extends Component{
     render(){
         return(
             <div className="services">
+            <img className="qrcode" src={qrcode} />
             <h2>Certified Mobile Notary Signing Agent travels to you.  Our service is strictly mobile by appointment only.</h2>
             <h2>Servicing the Queens, Brooklyn, Nassau and Suffolk Counties.</h2>
                 <ul>
